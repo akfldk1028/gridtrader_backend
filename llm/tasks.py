41be2e43cc11df725f1275b1b3805908eec45784
akfldk1028 <hanvit4303@gmail.com>
2024-08-29 15:42:03 +0900

    first commit

--- llm/tasks.py
+++ llm/tasks.py
@@ -34,7 +34,7 @@ def setup_bitcoin_analysis_task():
 
 
     # 다음 실행 시간을 오전 9시 10분으로 설정
-    next_hour = now.replace(hour=15, minute=36, second=0, microsecond=0)
+    next_hour = now.replace(hour=15, minute=50, second=0, microsecond=0)
 
     # 만약 현재 시간이 오늘 오전 9시 10분 이후라면, 다음 날로 설정
     if now > next_hour:
