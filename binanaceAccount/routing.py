5f2833b41f810efe2cb5e17af05d30e22a48a8f7
akfldk1028 <hanvit4303@gmail.com>
2024-08-31 11:02:40 +0900

    first commit

--- binanaceAccount/routing.py
+++ binanaceAccount/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
-from .consumers import BinanceWebSocketConsumer
+from .consumers import BinanceDataConsumer
 websocket_urlpatterns = [
-    re_path(r'ws/binance/$', BinanceWebSocketConsumer.as_asgi()),
+    re_path(r'ws/binance/$', BinanceDataConsumer.as_asgi()),
     # re_path(r'ws/markprice/$', MarkPriceConsumer.as_asgi()),
 
     # re_path(r'ws/account/$', OnDemandDataConsumer.as_asgi()),
